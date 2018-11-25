6bdc33a08bfc7ebf0b65d551ba2d106cab7d775b
Kashee Mistry <kam569@york.ac.uk>
2018-11-25 17:25:58 +0000

    revision tips

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -2373,7 +2373,5 @@ if (typeof jQuery === 'undefined') {
       Plugin.call($spy, data)
     })
   })
-  <div class="alert alert-warning" role="alert">
-    A simple warning alert with <a href="#" class="alert-link">an example link</a>. Give it a click if you like.
-  </div>
+
 }(jQuery);
